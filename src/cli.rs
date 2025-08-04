dcf36cdeac39078062c92e963e569d63e93a59bc
armando-codecafe <armando.roerdinkveldboom@code-cafe.nl>
2025-08-04 16:22:48 +0200

    fuzzy find cleanup

--- src/cli.rs
+++ src/cli.rs
@@ -41,7 +41,7 @@ impl Autocomplete for TextCompleter {
         return Ok(self
             .options
             .iter()
-            .filter(|t| t.contains(input))
+            .filter(|t| t.contains(&input.trim().replace(" ", "")))
             .map(|s| s.to_owned())
             .collect());
     }
@@ -54,7 +54,9 @@ impl Autocomplete for TextCompleter {
         if let Some(suggestion) = &highlighted_suggestion {
             return Ok(highlighted_suggestion);
         }
-        let suggestions = self.get_suggestions(input).unwrap();
+        let suggestions = self
+            .get_suggestions(&input.trim().replace(" ", ""))
+            .unwrap();
         let top_suggestion = suggestions.get(0);
         if let Some(suggestion) = top_suggestion {
             return Ok(Some(suggestion.clone()));
@@ -82,7 +84,12 @@ impl CliUtils {
     }
 
     pub fn template_fuzzy_find(templates: Vec<Template>) -> Result<String> {
-        let completer = TextCompleter::new(templates.iter().map(|t| t.info.name.clone()).collect());
+        let completer = TextCompleter::new(
+            templates
+                .iter()
+                .map(|t| format!("{}-{}", t.info.name.clone(), t.info.iteration))
+                .collect(),
+        );
         let template_name = Text::new("Please fill in the template name: ")
             .with_autocomplete(completer)
             .prompt();
